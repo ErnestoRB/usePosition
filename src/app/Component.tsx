221f00aa01c3fac98d667f4f593b4a0fd7df7d29
Ernesto Ramírez <ernestorb_@outlook.com>
2022-12-05 00:29:57 -0600

    Change the development demo app

--- src/app/Component.tsx
+++ src/app/Component.tsx
@@ -1,64 +1,127 @@
-import React from "react";
+import * as React from "react";
 import { useRef, useState } from "react";
 import { usePosition } from "../useposition";
 import "./style.css";
+import { useDrag } from "@use-gesture/react";
 
 export default function Component() {
   const ref = useRef<HTMLDivElement>(null);
+  const [callOnResize, setCallOnResize] = useState(true);
   const [open, setOpen] = useState(true);
-  const [offset, setOffset] = useState(0);
-
   const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+  const [childrenPos, setChildrenPos] = useState<{
+    left?: number;
+    top?: number;
+    bottom?: number;
+    right?: number;
+    transform: string;
+    display?: string;
+  }>({
+    left: 0,
+    top: 0,
+    transform: "translate(-100%, 50%)",
+  });
+
+  const bind = useDrag(
+    (state) => {
+      if (state.tap) {
+        console.log("tap");
+        setOpen(!open);
+      }
+      setX(state.offset[0]);
+      setY(state.offset[1]);
+    },
+    { filterTaps: true }
+  );
 
   usePosition(
     ref,
-    ({ left }, { windowResize, visible }) => {
+    (
+      { top, left, bottom, right },
+      { windowResize, visible, screenHeight, screenWidth }
+    ) => {
       if (windowResize) {
-        alert("Oops. You resized the window,");
+        console.log("Window resized and callback called");
       }
+
+      const positionFromBottom = screenHeight - bottom;
+      const positionFromRight = screenWidth - right;
+
+      const moreSpace = Math.max(
+        top,
+        left,
+        positionFromBottom,
+        positionFromRight
+      );
+
+      switch (moreSpace) {
+        case top:
+          setChildrenPos({
+            left: 0,
+            top: 0,
+            transform: "translate(50%, -100%)",
+            display: open ? "" : "none",
+          });
+          break;
+        case left:
+          setChildrenPos({
+            left: 0,
+            top: 0,
+            transform: "translate(-100%, 50%)",
+            display: open ? "" : "none",
+          });
+          break;
+        case positionFromRight:
+          setChildrenPos({
+            right: 0,
+            top: 0,
+            transform: "translate(100%, 50%)",
+            display: open ? "" : "none",
+          });
+          break;
+        case positionFromBottom:
+          setChildrenPos({
+            left: 0,
+            bottom: 0,
+            transform: "translate(50%, 100%)",
+            display: open ? "" : "none",
+          });
+          break;
+      }
+
       console.log(
-        `Position from left screen edge: ${left}`,
-        `State value: ${open}`,
+        `Right: ${positionFromRight}, Left: ${left}, Top: ${top}, Bottom: ${positionFromBottom}`,
+        `is Callin on resize: ${callOnResize}`,
         "Percent visible: " + visible
-      ); // here we declare what we want to do
+      );
     },
-    undefined,
-    [open]
+    { callOnResize }, // pretty expensive
+    [callOnResize, open] // if I dont declare open state then the function would have a old valaue
   );
 
   return (
     <div id="container">
-      <label htmlFor="offset">Pixel offset (how much the block moves)</label>
-      <input
-        name="offset"
-        type="number"
-        value={offset}
-        onChange={(e) => setOffset(Number(e.target.value))}
-      ></input>
-      <button
-        type="button"
-        data-testid="button"
-        onClick={() => {
-          setX((x) => x + offset);
-        }}
-      >
-        Move
-      </button>
+      <h1>Move the red block!</h1>
+      <p>Try resizing the window</p>
       <button
         type="button"
         onClick={() => {
-          setOpen((x) => !x);
+          setCallOnResize(!callOnResize);
         }}
       >
-        Change state deps
+        Toggle callback being called when window's resized
       </button>
       <div className="block-container">
         <div
+          {...bind()}
           className="block"
-          data-testid="block"
-          style={{ left: x }}
+          style={{ left: x, top: y }}
           ref={ref}
-        />
+        >
+          Click to hide my child!
+          <div className="block-sibling" style={childrenPos}></div>
+        </div>
       </div>
     </div>
   );
