895a0c2286ac1043b994efa0251750fdbe7b2397
Ernesto Ramírez <ernestorb_@outlook.com>
2022-12-20 22:30:23 -0600

    If window is undefined then do nothing

--- src/useposition.tsx
+++ src/useposition.tsx
@@ -45,75 +45,78 @@ function generateID() {
   const n2 = Date.now();
   return `id-${n1.toString(16)}-${n2.toString(10)}`;
 }
-
 const clients: Array<PositionReg> = [];
 
-let oldWidth = window.innerWidth;
-let oldHeight = window.innerHeight;
-let lastExecute = 0;
+(function () {
+  if (typeof window === "undefined") return;
 
-const watch = (time: DOMHighResTimeStamp) => {
-  if (time - lastExecute < 100) {
-    window.requestAnimationFrame(watch);
-    return;
-  }
-  lastExecute = time;
-  const actualWindowWidth = window.innerWidth;
-  const actualWindowHeight = window.innerHeight;
-  const windowResize =
-    oldWidth !== actualWindowWidth || oldHeight !== actualWindowHeight;
+  let oldWidth = window.innerWidth;
+  let oldHeight = window.innerHeight;
+  let lastExecute = 0;
 
-  clients.forEach((r) => {
-    if (!r.ref || !r.cb) return;
-    const rect = r.ref.getBoundingClientRect();
-    if (!r.prevVal) {
-      r.prevVal = rect;
+  const watch = (time: DOMHighResTimeStamp) => {
+    if (time - lastExecute < 100) {
+      window.requestAnimationFrame(watch);
       return;
     }
+    lastExecute = time;
+    const actualWindowWidth = window.innerWidth;
+    const actualWindowHeight = window.innerHeight;
+    const windowResize =
+      oldWidth !== actualWindowWidth || oldHeight !== actualWindowHeight;
 
-    const { top, left } = rect;
-    const hasMoved = r.prevVal.top !== top || r.prevVal.left !== left;
-    const elementArea = rect.width * rect.height;
-    function between(number: number, min: number, max: number) {
-      return Math.min(max, Math.max(min, number));
-    }
-    const visibleArea =
-      (between(rect.right, 0, actualWindowWidth) -
-        between(rect.left, 0, actualWindowWidth)) *
-      (between(rect.bottom, 0, actualWindowHeight) -
-        between(rect.top, 0, actualWindowHeight));
-    const visibleThreshold = visibleArea / elementArea;
-    const extraValuesObject = {
-      windowResize,
-      screenWidth: actualWindowWidth,
-      screenHeight: actualWindowHeight,
-      visible: visibleThreshold,
-    };
-    if (!hasMoved) {
-      if (r.config.callOnResize && windowResize) {
-        r.cb(rect, extraValuesObject);
+    clients.forEach((r) => {
+      if (!r.ref || !r.cb) return;
+      const rect = r.ref.getBoundingClientRect();
+      if (!r.prevVal) {
+        r.prevVal = rect;
         return;
       }
-      if (r.depsChange) {
-        r.depsChange = false;
-        r.cb(rect, extraValuesObject);
-      }
-      return;
-    }
-    r.prevVal = rect;
-    r.cb(rect, extraValuesObject);
-  });
-  if (windowResize) {
-    oldWidth = actualWindowWidth;
-    oldHeight = actualWindowHeight;
-  }
-  window.requestAnimationFrame(watch);
-};
 
-window.requestAnimationFrame((time) => {
-  lastExecute = time;
-  watch(time);
-});
+      const { top, left } = rect;
+      const hasMoved = r.prevVal.top !== top || r.prevVal.left !== left;
+      const elementArea = rect.width * rect.height;
+      function between(number: number, min: number, max: number) {
+        return Math.min(max, Math.max(min, number));
+      }
+      const visibleArea =
+        (between(rect.right, 0, actualWindowWidth) -
+          between(rect.left, 0, actualWindowWidth)) *
+        (between(rect.bottom, 0, actualWindowHeight) -
+          between(rect.top, 0, actualWindowHeight));
+      const visibleThreshold = visibleArea / elementArea;
+      const extraValuesObject = {
+        windowResize,
+        screenWidth: actualWindowWidth,
+        screenHeight: actualWindowHeight,
+        visible: visibleThreshold,
+      };
+      if (!hasMoved) {
+        if (r.config.callOnResize && windowResize) {
+          r.cb(rect, extraValuesObject);
+          return;
+        }
+        if (r.depsChange) {
+          r.depsChange = false;
+          r.cb(rect, extraValuesObject);
+        }
+        return;
+      }
+      r.prevVal = rect;
+      r.cb(rect, extraValuesObject);
+    });
+    if (windowResize) {
+      oldWidth = actualWindowWidth;
+      oldHeight = actualWindowHeight;
+    }
+    window.requestAnimationFrame(watch);
+  };
+
+  window.requestAnimationFrame((time) => {
+    lastExecute = time;
+    watch(time);
+  });
+})();
 
 const defaultConfig = { callOnResize: true };
 
@@ -134,6 +137,7 @@ export function usePosition(
   const clientRef = useRef<PositionReg | undefined>();
 
   useEffect(() => {
+    if (typeof window === "undefined") return;
     const idGen = generateID();
     const object = {
       id: idGen,
